2913c365b63abf46068bcb982fd7747620c13a6c
Istvan Meszaros <istvan.meszaros.88@gmail.com>
2020-07-30 11:14:42 +0200

    working simple solution

--- storageservice/src/api.py
+++ storageservice/src/api.py
@@ -6,11 +6,20 @@ import os
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = get_config('storage','upload_path')
 
+@app.route('/')
+def health_check():
+    return 'OK'
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     f = request.files['file']
     fname = secure_filename(f.filename)
     path = os.path.join(app.config['UPLOAD_FOLDER'], fname)
     f.save(path)
-    return f'Uploaded to: {path}'
-    
+    return f'Uploaded to:{path}'
+
+@app.route('/list_uploaded')
+def list_uploaded():
+    path = app.config['UPLOAD_FOLDER']
+    files_and_dirs = [f for f in os.listdir(path)]
+    return files_and_dirs
